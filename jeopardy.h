622afd9f04d3eab11fc09083ab2a0411a558bbca
GayanathS <gayanath.sasitharanathan@uoit.net>
2017-02-17 15:08:02 +0000

    Fixed external boolean

--- jeopardy.h
+++ jeopardy.h
@@ -13,7 +13,7 @@
 // Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
 extern void tokenize(char *input, char **tokens);
 
-extern_bool is_valid_answer(char ** tokens);
+extern bool is_valid_answer(char ** tokens);
 // Displays the game results for each player, their name and final score, ranked from first to last place
 extern void show_results(player *players, int num_players);
 
